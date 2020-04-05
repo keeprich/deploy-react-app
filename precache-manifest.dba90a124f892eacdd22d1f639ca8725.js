e72f73df3b78354f14a2d6c0fee28a59b73bd787
keeprich <keeprich@keeprichs-air.lan>
2020-04-05 17:07:10 +0100

    Updates

---
+++ precache-manifest.dba90a124f892eacdd22d1f639ca8725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e014ad5ba333b90d0db10150c49470ec",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "54ed7fa41c63cd4fbe7c",
+    "url": "/deploy-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4f69bc05ac09ddc1251",
+    "url": "/deploy-react-app/static/js/2.6f5989bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react-app/static/js/2.6f5989bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ed7fa41c63cd4fbe7c",
+    "url": "/deploy-react-app/static/js/main.55fa52bb.chunk.js"
+  },
+  {
+    "revision": "887c1fe4c51b319209e5",
+    "url": "/deploy-react-app/static/js/runtime-main.4ae991c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
